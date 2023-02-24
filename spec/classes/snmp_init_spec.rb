0662f0c9b2225bbb9c725feb54668eb344dbfc83
Skylar Thompson <skylar2@uw.edu>
2023-02-24 14:44:20 -0800

    extend 20.04 snmp_init_spec test cases to include 22.04

--- spec/classes/snmp_init_spec.rb
+++ spec/classes/snmp_init_spec.rb
@@ -607,7 +607,7 @@ describe 'snmp' do
           ).that_requires('Package[snmpd]').that_notifies('Service[snmpd]')
         }
         case facts[:os]['release']['major']
-        when '8', '9', '10', '16.04', '18.04', '20.04'
+        when '8', '9', '10', '16.04', '18.04', '20.04', '22.04'
           it {
             is_expected.to contain_file('snmptrapd.sysconfig').with(
               ensure: 'present',
@@ -674,7 +674,7 @@ describe 'snmp' do
               ).that_requires('Package[snmpd]')
             }
           end
-        when '18.04', '20.04'
+        when '18.04', '20.04', '22.04'
           describe 'Debian-snmp as snmp user' do
             it 'contains File[snmpd.sysconfig] with contents "SNMPDOPTS="-Lsd -Lf /dev/null -u Debian-snmp -g Debian-snmp -I -smux -p /var/run/snmpd.pid""' do
               verify_contents(catalogue, 'snmpd.sysconfig', [
